c87f032272573c49fccfb505c187567bddec52b9
Greeshma Thattepally <50731579+greeshmathattepally@users.noreply.github.com>
2023-07-10 22:24:53 +0530

    Add files via upload
    CMS backend code

---
+++ springbootcms/CMS-1/src/main/java/com/example/demo/exception/GlobalExecptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.demo.exception;
+import com.example.demo.model.ErrorResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+@RestControllerAdvice
+public class GlobalExecptionHandler {
+    
+    @ExceptionHandler(QuestionNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse questionNotFoundException(QuestionNotFoundException ex) {
+        return new ErrorResponse(
+                ex.errorCode,
+                ex.message,
+                HttpStatus.BAD_REQUEST.value());
+    }
+
+}
